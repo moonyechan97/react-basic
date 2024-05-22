4da3ca78ebb43968d2f9e3b5aa1f050e4c63f476
DESKTOP-O3LMC10\User <skyfiy97@naver.com>
2024-05-22 09:03:30 +0900

    0521

---
+++ src/components/ClassKakaoMap.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class KakaoMap extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+      
+        return (
+            <div>
+               <h2><a href="/">클래스형 전기차 충전소 위치</a></h2>
+            </div>
+        );
+    }
+}
+
+KakaoMap.propTypes = {
+
+};
+
+export default KakaoMap;
